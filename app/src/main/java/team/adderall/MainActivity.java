5aebc877f2d463a1719dccb1d1cc9d0d6df75f8f
Cim Stordal <cim96@hotmail.com>
2018-04-28 21:46:25 +0200

    Change username when logged in

--- app/src/main/java/team/adderall/MainActivity.java
+++ app/src/main/java/team/adderall/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -96,6 +97,7 @@ public class MainActivity
             }
         });
 
+        updateMenuToSignedIn(false);
 
         LOGGER.setLevel(Level.INFO);
     }
@@ -146,8 +148,32 @@ public class MainActivity
     public void onGetGplayInteraction(GoogleSignInAccount acc) {
         this.gplay = acc;
         this.gplayAcc = new GooglePlay(this,acc);
+        if(acc != null) {
+            updateMenuToSignedIn(true);
+            updateUsername(acc.getDisplayName());
+        }
+        else{
+            this.gplayAcc = null;
+            updateMenuToSignedIn(false);
+            updateUsername("");
+
+        }
 
     }
+
+    private void updateUsername(String displayName) {
+        NavigationView nv = findViewById(R.id.nav_view);
+        TextView username = nv.getHeaderView(0).findViewById(R.id.headerName);
+        username.setText(displayName);
+    }
+
+    private void updateMenuToSignedIn(boolean b) {
+        String title = b == true?  "Logout" : "Login";
+        NavigationView nv = findViewById(R.id.nav_view);
+        MenuItem login =  nv.getMenu().getItem(4);
+        login.setTitle(title);
+    }
+
     @Override
     public void startGoogleHighscoreView() {
         showLeaderboard();
--- app/src/main/java/team/adderall/fragments/LogoutLoginFragment.java
+++ app/src/main/java/team/adderall/fragments/LogoutLoginFragment.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
@@ -20,12 +22,29 @@ public class LogoutLoginFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Intent i = new Intent(this.getContext(), GoogleSignInActivity.class);
-        startActivityForResult(i, 1);
+        FragmentListner fListner = (FragmentListner) this.getActivity();
+
+        if(!fListner.isLoggedIn()) {
+            Intent i = new Intent(this.getContext(), GoogleSignInActivity.class);
+            startActivityForResult(i, 1);
+        }
+        else{
+            fListner.onGetGplayInteraction(null);
+            startMain(false);
+        }
 
         return inflater.inflate(R.layout.logout_login_view, container, false);
     }
+    private void startMain(boolean loggedIn){
+        String status = loggedIn == false ? "Logged out!" : "Logged In!";
+        Toast.makeText(this.getContext(),"You are now " + status,Toast.LENGTH_LONG).show();
 
+        SoloFragment fragment = new SoloFragment();
+        FragmentManager fragmentManager = this.getActivity().getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+    }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -33,12 +52,13 @@ public class LogoutLoginFragment
             if(resultCode == Activity.RESULT_OK){
                 FragmentListner b = (FragmentListner) this.getActivity();
 
-                GoogleSignInAccount result= data.getParcelableExtra("result");
+                GoogleSignInAccount result = data.getParcelableExtra("result");
 
 
 
                 b.onGetGplayInteraction(result);
-                //b.startGoogleHighscoreView();
+                startMain(true);
+
 
             }
             if (resultCode == Activity.RESULT_CANCELED) {
